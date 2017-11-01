a63a8483bf41950202138eca8d0e2aeb96d097e0
Le Cong Chinh <lecongchinh.ptpt@gmail.com>
2017-11-01 09:06:22 +0700

    fix

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,6 +4,49 @@ import Todo from './Todo'
 import axios from 'axios'
 import {getTodo} from '../actions'
 
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos,
+            onDeleteClick,
+            onEditClick
+        }
+    }
+
+    componentDidMount() {
+        let {dispatch} = this.props;
+        axios({
+            method: 'get',
+            url: 'http://localhost:8000'
+        })
+            .then((res) => res.data)
+            .then((datas) => dispatch(getTodo(datas.map(data => this.data))))
+    }
+
+    render() {
+        return(
+            <ul>
+                {this.state.todos.map(todo => (
+                    <Todo
+                        onDeleteClick={() => {
+                            onDeleteClick(todo.id);
+                            deleteTodoDatabase(todo.id)
+                        }}
+
+                        onEditClick = {() => {
+                            let text = prompt('Change text', todo.text);
+                            onEditClick(todo.id,text);
+                        }}
+
+                        key={todo.id} {...todo}/>
+                ))}
+            </ul>
+        )
+    }
+
+}
+
 function deleteTodoDatabase(id) {
     axios({
         method: 'delete',
@@ -11,43 +54,35 @@ function deleteTodoDatabase(id) {
     })
 }
 
-function componentDidMount() {
-    let {dispatch} = this.props;
-    axios({
-        method: 'get',
-        url: 'http://localhost:8000'
-    })
-    .then((res) => res.data)
-    .then((datas) => dispatch(getTodo(datas.map(data => this.data))))
-}
 
-const TodoList = ({todos, onDeleteClick, onEditClick}) => (
-    <ul>
-        {todos.map(todo => (
-            <Todo
-                onDeleteClick={() => {
-                    onDeleteClick(todo.id);
-                    deleteTodoDatabase(todo.id)
-                }}
 
-                  onEditClick = {() => {
-                      let text = prompt('Change text', todo.text);
-                      onEditClick(todo.id,text);
-                  }}
+// const TodoList1 = ({todos, onDeleteClick, onEditClick}) => (
+//     <ul>
+//         {todos.map(todo => (
+//             <Todo
+//                 onDeleteClick={() => {
+//                     onDeleteClick(todo.id);
+//                     deleteTodoDatabase(todo.id)
+//                 }}
+//
+//                   onEditClick = {() => {
+//                       let text = prompt('Change text', todo.text);
+//                       onEditClick(todo.id,text);
+//                   }}
+//
+//                   key={todo.id} {...todo}/>
+//         ))}
+//     </ul>
+// );
 
-                  key={todo.id} {...todo}/>
-        ))}
-    </ul>
-);
-
-TodoList.propTypes = {
-    todos: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            completed: PropTypes.bool.isRequired,
-            text: PropTypes.string.isRequired
-        }).isRequired
-    ).isRequired,
-};
+// TodoList.propTypes = {
+//     todos: PropTypes.arrayOf(
+//         PropTypes.shape({
+//             id: PropTypes.number.isRequired,
+//             completed: PropTypes.bool.isRequired,
+//             text: PropTypes.string.isRequired
+//         }).isRequired
+//     ).isRequired,
+// };
 
 export default TodoList
